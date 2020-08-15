d0898069ff6504dd5e6c0d4caff068063feb6b70
lukendev <lucas.azevedo2104@gmail.com>
2020-08-15 14:38:04 -0300

    feat: Sequential Structure - Ex 2

---
+++ ex-2/ex-2/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		System.out.println("SOMA = " + (x+y));
+		
+		sc.close();
+
+	}
+
+}
