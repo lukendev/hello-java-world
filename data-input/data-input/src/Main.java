98894f8ebc59ed2569d4421d21329bc3bd3cfeb7
lukendev <lucas.azevedo2104@gmail.com>
2020-08-15 14:05:26 -0300

    feat: Data input part 2

--- data-input/data-input/src/Main.java
+++ data-input/data-input/src/Main.java
@@ -5,19 +5,22 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		String x;
-		int y;
-		double z;
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
-		System.out.println("Dados digitados: ");
+		int x;
+		String s1, s2, s3;
+		
+		x = sc.nextInt();
+		sc.nextLine();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("DADOS DIGITADOS: ");
 		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 		
 		sc.close();
 
